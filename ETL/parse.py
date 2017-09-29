45b43c44043cd56ca9a85598cf2414d11780d650
Prajwal Prasan <prajwal.prasan@gmail.com>
2017-09-28 20:09:54 -0700

    Updated parse.py

--- ETL/parse.py
+++ ETL/parse.py
@@ -1,24 +1,17 @@
-import csv
-notToInclude = {2,3,4,7} #columns not to include
-with open("pricing.csv", "rb") as f:
-	with open("modified.csv", "w") as f1:
-		reader = csv.reader(f, delimiter=",")
-		for i, line in enumerate(reader):
-			for j in range(len(line)):
-				if j in notToInclude: continue #skip these columns
-				if j==5: #ORIG
-					f1.write("orig" + ',')#replace with mapping to city/country name
-				if j==6: #DEST
-					f1.write("dest" + ',')#replace with mapping to city/country name
-				if j==8: #class need to map to 1,2,3. Y-Economy, what is business and first?
-					f1.write("class" + ',')
-				else:
-					f1.write(line[j] + ',')
-			f1.write('\n')
+import pandas as pd
+from datetime import datetime
+
+pricingDF = pd.read_csv("../data/emirates/pricing.csv")
+
+with open("../data/emirates/parsedPricingData.csv", "w") as outputFile:
+    for i, row in pricingDF.iterrows():
+        departureDate = datetime.strptime(row["DEPARTURE_DATE"], "%m/%d/%y")
+        outputFile.write(str(departureDate.year) +
+                         "\n")
 
 
-#why does DOW have many numbers?
+# why does DOW have many numbers?
 
 
-#Assumptions:
-#All fare in USD
+# Assumptions:
+# All fare in USD
