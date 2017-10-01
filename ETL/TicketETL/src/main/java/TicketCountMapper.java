d7869627e4a1cce7dccf109ad2cd3a7c01b901c4
Prajwal Prasan <prajwal.prasan@gmail.com>
2017-10-01 03:29:25 -0700

    Added Mapper and Reducer

--- ETL/TicketETL/src/main/java/TicketCountMapper.java
+++ ETL/TicketETL/src/main/java/TicketCountMapper.java
@@ -1,8 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Pattern;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 public class TicketCountMapper {
     private static PrintWriter out = Utils.utfStdOut();
-    public static void main() {
+    private static final Pattern csvPattern = Pattern.compile(",");
+    private static final Pattern datePattern = Pattern.compile(" ");
 
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+            String line;
+            while((line = br.readLine()) != null) {
+                try {
+                    String[] columns = csvPattern.split(line);
+                    String orig = columns[8];
+                    String dest = columns[10];
+                    String departureDateTime = columns[14];
+
+                    if (isBlank(orig) || isBlank(dest) || isBlank(departureDateTime))
+                        continue;
+
+                    String departureDate = datePattern.split(departureDateTime)[0];
+                    out.println(orig + ":" + dest + ":" + departureDate +
+                            "\t" + "1");
+                }
+                catch (Exception e) {}
+
+            }
+        }
+        catch (Exception e) {
+            //e.printStackTrace();
+        }
     }
 }
--- ETL/TicketETL/src/main/java/TicketCountReducer.java
+++ ETL/TicketETL/src/main/java/TicketCountReducer.java
@@ -1,5 +1,35 @@
-public class TicketCountReducer {
-    public static void main() {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.regex.Pattern;
 
+public class TicketCountReducer {
+    private static PrintWriter out = Utils.utfStdOut();
+    private static final Pattern tabSeparator = Pattern.compile("\t");
+    private static final Pattern keySeparator = Pattern.compile(":");
+    private static HashMap<String, Long> map = new HashMap<>();
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+            String line;
+            while((line = br.readLine()) != null) {
+                try {
+                    String[] lineEntries = tabSeparator.split(line);
+                    String key = lineEntries[0];
+                    Long val = Long.parseLong(lineEntries[1]);
+                    map.put(key, map.getOrDefault(key, 0L) + val);
+                }
+                catch (Exception e) {}
+            }
+            map.forEach((key, val) -> {
+                String[] keys = keySeparator.split(key);
+                out.println(keys[0] + "," + keys[1] + "," + keys[2] + "," + val);
+            });
+        }
+        catch (Exception e) {
+            //e.printStackTrace();
+        }
     }
 }
